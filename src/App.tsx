03fdcbf73c377897c4ac5aa45c11c4b7cfe30818
Matheussmaced <mateusbyakuya3@gmail.com>
2024-01-08 23:17:37 -0300

    fix: import in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/@themes/default'
 import { GlobalStyles } from './styles/global'
-import { HeaderComponent } from './components/Header'
-import { BodyComponent } from './components/Body'
+import { HeaderComponent } from './components/Header/index'
+import { BodyComponent } from './components/Body/index'
 
 function App() {
   return (
